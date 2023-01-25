551214f827d8b5adffee09b1892280d06b48584b
Abdul535 <syedabdullah535@gmail.com>
2023-01-25 11:01:09 +0530

    tCommit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import useSWR from 'swr';
 import { Doughnut } from 'react-chartjs-2';
+import { useEffect, useState } from 'react';
 
 import {
 	Chart as ChartJS,
@@ -29,15 +30,13 @@ ChartJS.register(
 	Legend
 );
 
-
 export async function getServerSideProps() {
 	const res = await fetch('https://www.contrahacker.com/api/example-data');
 	const data = await res.json();
 
-
 	return {
 		props: {
-			data
+			data,
 		},
 	};
 }
@@ -45,21 +44,17 @@ export async function getServerSideProps() {
 const inter = Inter({ subsets: ['latin'] });
 
 // https://www.contrahacker.com/api/example-data
-
-const fetcher = async ()=>{
-	const res = await fetch('https://www.contrahacker.com/api/example-data');
+// https://personal-website-contrahacker.vercel.app/api/example-data
+const fetcher = async () => {
+	const res = await fetch(
+		'https://personal-website-contrahacker.vercel.app/api/example-data'
+	);
 	const data = await res.json();
-  return data;
-}
+	// console.log('try', data);
+	return data;
+};
 
-export default function Home({ sdata }) {
-  const {data, error} = useSWR('dashboard', fetcher,{
-    initialData: sdata
-  })
-
-  if(error) return 'An error has occured'
-  if(!data) return 'Loading'
-
+const genChart = (data) => {
 	let final = data.data.reduce((acc, curr) => {
 		if (acc[curr.sector]) {
 			acc[curr.sector] += curr.price;
@@ -77,10 +72,10 @@ export default function Home({ sdata }) {
 	});
 
 	const dn = {
-    labels: Object.keys(final),
+		labels: Object.keys(final),
 		datasets: [
 			{
-				label: "count",
+				label: 'count',
 				data: Object.values(final),
 				backgroundColor: ['orange', 'blue', 'green', 'red'],
 				borderWidth: 0,
@@ -94,11 +89,26 @@ export default function Home({ sdata }) {
 				label: '',
 				data: final1.map((ele) => ele.price),
 				backgroundColor: ['orange', 'blue', 'green', 'red'],
-
 			},
 		],
 	};
+	return [dn, bar];
+};
 
+export default function Home({ sdata }) {
+	let err = false;
+	const { data, error } = useSWR('dashboard', fetcher, {
+		initialData: sdata,
+	});
+
+	if (data) {
+		// console.log('comp', data);
+		if (data.hasOwnProperty('error')) {
+			err = true;
+		} else {
+			var [dn, bar] = genChart(data);
+		}
+	}
 	return (
 		<>
 			<Head>
@@ -125,154 +135,56 @@ export default function Home({ sdata }) {
       }
       {data.dataset} */}
 
-      <div className={styles.body}>
+			<div className={styles.body}>
+				<main className={styles.main}>
+					{!data ? (
+						<h1>Loading...{data?.error}</h1>
+					) : data?.error ? (
+						<h1>{data.error.message}</h1>
+					) : (
+						<>
+							<div className={styles.dn}>
+								<Doughnut
+									data={dn}
+									options={{
+										responsive: true,
+										plugins: {
+											legend: {
+												display: false,
+											},
+											title: {
+												display: true,
+												text: 'Portfolio Distribution into Sectors',
+												position: 'bottom',
+											},
+										},
+									}}
+								/>
+							</div>
 
-			<main className={styles.main}>
-				<div className={styles.dn}>
-					<Doughnut
-						data={dn}
-						options={{
-              responsive: true,
-							plugins: {
-                legend:{
-                  display:false
-                },
-								title: {
-									display: true,
-									text: 'Portfolio Distribution into Sectors',
-                  position: 'bottom',
-								},
-							},
-						}}
-					/>
-				</div>
-
-				<div className={styles.bar}>
-					<Bar
-						data={bar}
-						options={{
-              maintainAspectRatio:false,
-              responsive: true,
-							plugins: {
-                legend:{
-                  display:false
-                },
-								title: {
-                  display: true,
-                  text: 'Price (USD)',
-                  position: 'bottom',
-								},
-							},
-						}}
-					/>
-				</div>
-			</main>
-
-      </div>
-			{/* <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main> */}
+							<div className={styles.bar}>
+								<Bar
+									data={bar}
+									options={{
+										maintainAspectRatio: false,
+										responsive: true,
+										plugins: {
+											legend: {
+												display: false,
+											},
+											title: {
+												display: true,
+												text: 'Price (USD)',
+												position: 'bottom',
+											},
+										},
+									}}
+								/>
+							</div>
+						</>
+					)}
+				</main>
+			</div>
 		</>
 	);
 }
